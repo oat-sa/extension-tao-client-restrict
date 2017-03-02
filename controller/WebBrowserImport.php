8cd96e21b6270830ae4a2e5d961b9bfd45d62102
antoinerobin <antoine.robin.2014@utt.fr>
2017-03-02 10:37:24 +0100

    custom importer for client restrict resources

---
+++ controller/WebBrowserImport.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+namespace oat\taoClientRestrict\controller;
+
+use oat\taoClientRestrict\model\import\WebBrowserRdfImporter;
+
+/**
+ * This controller provide the actions to import items 
+ * 
+ * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * @package taoItems
+ 
+ *
+ */
+class WebBrowserImport extends \tao_actions_Import {
+
+
+
+    protected function getAvailableImportHandlers() {
+        return array(
+            new WebBrowserRdfImporter(),
+        );
+    }
+
+
+}
