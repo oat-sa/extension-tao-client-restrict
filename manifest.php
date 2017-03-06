83f2181ee0ff40d015526dd74f3b8c151a7084e7
antoinerobin <antoine.robin.2014@utt.fr>
2017-03-06 12:13:40 +0100

    update version number

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
     'label' => 'Client Restrictions',
     'description' => '',
     'license' => 'GPL-2.0',
-    'version' => '1.3.0',
+    'version' => '1.3.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=7.74.0',
