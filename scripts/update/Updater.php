d5d855560a0926cbfdb7e8aa3ba7f9cf7d685bc5
Ionut Padurariu <ionut@taotesting.com>
2017-12-11 16:37:46 +0100

    increase version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -76,7 +76,7 @@ class Updater extends common_ext_ExtensionUpdater {
             $this->setVersion('2.2.0');
         }
 
-        $this->skip('2.2.0', '3.1.2');
+        $this->skip('2.2.0', '3.2.0');
     }
 
 }
