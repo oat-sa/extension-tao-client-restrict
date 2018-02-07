57bf02b5aa22f9f91681ceb3cfc43eface428188
jsconan <jsconan@gmail.com>
2018-02-07 09:35:33 +0100

    Fix diagnostics

--- views/js/diagnosticTools/os/tester.js
+++ views/js/diagnosticTools/os/tester.js
@@ -13,20 +13,20 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2016 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2016-2017 (original work) Open Assessment Technologies SA ;
  */
 /**
- * @author Jean-Sébastien Conan <jean-sebastien.conan@vesperiagroup.com>
+ * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>
  */
 define([
     'jquery',
     'lodash',
     'i18n',
     'util/url',
-    'taoClientDiagnostic/tools/getconfig',
+    'taoClientDiagnostic/tools/getConfig',
+    'taoClientDiagnostic/tools/getLabels',
     'taoClientDiagnostic/tools/getPlatformInfo'
-
-], function ($, _, __, url, getConfig, getPlatformInfo) {
+], function ($, _, __, url, getConfig, getLabels, getPlatformInfo) {
     'use strict';
 
     /**
@@ -35,6 +35,7 @@ define([
      * @private
      */
     var _defaults = {
+        id: 'os_version',
         osVersionAction: 'diagnose',
         osVersionController: 'OS',
         osVersionExtension: 'taoClientRestrict'
@@ -50,53 +51,57 @@ define([
         APPROVED_OS: '%APPROVED_OS%'
     };
 
+    /**
+     * List of translated texts per level.
+     * The level is provided through the config as a numeric value, starting from 1.
+     * @type {Object}
+     * @private
+     */
+    var _messages = [
+        // level 1
+        {
+            title: __('Operating system version'),
+            status: __('Checking the os version...'),
+            success: __('Pass – Your Operating System is approved'),
+            failure: __('Issue'),
+            compatible: __('Compatible'),
+            notCompatible: __('Not Compatible')
+        }
+    ];
 
     /**
-     * Performs a operating system support test
+     * Performs a browser support test
      *
-     * @param {Object} [config] - Some optional configs
-     * @param {String} [config.action] - The name of the action to call to get the os checker
-     * @param {String} [config.controller] - The name of the controller to call to get the os checker
-     * @param {String} [config.extension] - The name of the extension containing the controller to call to get the os checker
+     * @param {Object} config - Some optional configs
+     * @param {String} [config.id] - The identifier of the test
+     * @param {String} [config.action] - The name of the action to call to get the browser checker
+     * @param {String} [config.controller] - The name of the controller to call to get the browser checker
+     * @param {String} [config.extension] - The name of the extension containing the controller to call to get the browser checker
+     * @param {String} [config.level] - The intensity level of the test. It will aim which messages list to use.
      * @returns {Object}
      */
-    function osTester(config, diagnosticTool) {
-        var initConfig = getConfig(config || {}, _defaults);
+    function osTester(config) {
+        var initConfig = getConfig(config, _defaults);
+        var labels = getLabels(_messages, initConfig.level);
 
         return {
             /**
-             * Performs a os support test, then call a function to provide the result
+             * Performs a browser support test, then call a function to provide the result
              * @param {Function} done
              */
             start: function start(done) {
                 var testerUrl = url.route(initConfig.osVersionAction, initConfig.osVersionController, initConfig.osVersionExtension);
-
-                diagnosticTool.changeStatus(__('Checking the os version...'));
+                var self = this;
 
                 getPlatformInfo(window)
                     .then(function(platformInfo) {
                         $.ajax({
-                            url : testerUrl,
-                            success : function(data) {
+                            url: testerUrl,
+                            success: function (data) {
                                 var percentage = data.success ? 100 : 0;
-                                var status = {
-                                    percentage: percentage,
-                                    quality: {},
-                                    feedback: {
-                                        message: data.success ? __('Pass – Your Operating System is approved') : __('Issue'),
-                                        threshold: 100,
-                                        type: data.success ? 'success' : 'error'
-                                    }
-                                };
-                                var summary = {
-                                    os: {
-                                        message: __('Operating system version'),
-                                        value: data.success ? __('Compatible') : __('Not Compatible')
-                                    }
-                                };
+                                var status = self.getFeedback(percentage, data);
+                                var summary = self.getSummary(platformInfo, data);
                                 var currentOs = platformInfo.os + ' ' + platformInfo.osVersion;
-                                var customMsg = diagnosticTool.getCustomMsg('diagOsCheckResult') || '';
-
                                 var approvedOs = [];
 
                                 if (_.isObject(data.approvedOs)) {
@@ -108,18 +113,62 @@ define([
                                     });
                                 }
 
-                                status.id = 'os_version';
-                                status.title = __('Operating system version');
-
-                                customMsg = customMsg
-                                    .replace(_placeHolders.CURRENT_OS, currentOs)
-                                    .replace(_placeHolders.APPROVED_OS, approvedOs.join(', '));
-                                diagnosticTool.addCustomFeedbackMsg(status, customMsg);
+                                status.customMsgRenderer = function(customMsg) {
+                                    return (customMsg || '')
+                                        .replace(_placeHolders.CURRENT_OS, currentOs)
+                                        .replace(_placeHolders.APPROVED_OS, approvedOs.join(', '));
+                                };
 
                                 done(status, summary, {compatible: data.success});
                             }
                         });
                     });
+            },
+
+            /**
+             * Gets the labels loaded for the tester
+             * @returns {Object}
+             */
+            get labels() {
+                return labels;
+            },
+
+            /**
+             * Builds the results summary
+             * @param {Object} results
+             * @param {Object} data
+             * @returns {Object}
+             */
+            getSummary: function getSummary(results, data) {
+                return {
+                    os: {
+                        message: labels.title,
+                        value: data.success ? labels.compatible : labels.notCompatible
+                    }
+                };
+            },
+
+            /**
+             * Gets the feedback status for the provided result value
+             * @param {Number} result
+             * @param {Object} data
+             * @returns {Object}
+             */
+            getFeedback: function getFeedback(result, data) {
+                var status = {
+                    percentage: result,
+                    quality: {},
+                    feedback: {
+                        message: data.success ? labels.success : labels.failure,
+                        threshold: 100,
+                        type: data.success ? 'success' : 'error'
+                    }
+                };
+
+                status.id = initConfig.id;
+                status.title =  labels.title;
+
+                return status;
             }
         };
     }
